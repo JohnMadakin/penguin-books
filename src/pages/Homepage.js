63439823b21879a5eff7d4dd8a7a74fdb18a3b8c
johnmadakin <edafeadjeke@gmail.com>
2020-06-20 03:08:19 +0100

    homepage

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -23,7 +23,7 @@ export default function Homepage() {
                   <span className="text-primary-color"> Soul</span>
                 </h2>
                 <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
-                  Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.
+                  A webapp to manage your library.
                 </p>
                 <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
                   <div className="rounded-md shadow">
